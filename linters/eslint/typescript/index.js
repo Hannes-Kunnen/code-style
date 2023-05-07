9f66e035c58a8e1e4446c155a2203eb2cb22935a
Hannes Kunnen <hannes.kunnen1999@gmail.com>
2023-05-08 00:42:26 +0200

    Remove no-type-alias

--- linters/eslint/typescript/index.js
+++ linters/eslint/typescript/index.js
@@ -124,7 +124,6 @@ module.exports = {
             },
         ],
         '@typescript-eslint/no-throw-literal': 'error',
-        '@typescript-eslint/no-type-alias': 'error',
         '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
         '@typescript-eslint/no-unnecessary-condition': 'error',
         '@typescript-eslint/no-unnecessary-qualifier': 'error',
